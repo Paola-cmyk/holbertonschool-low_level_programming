f90c6348a5bde842436bdd204324f412f6d3425c
Paola-cmyk <paonicolegonzalez@gmail.com>
2025-02-21 13:26:50 -0400

    6 is worth it

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - shows the value of an integer
+ * @n: the int to check
+ * Return:the absolute value of in
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
