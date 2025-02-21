4af764f1804e19b9806583182c02bf787e2cadff
Paola-cmyk <paonicolegonzalez@gmail.com>
2025-02-21 11:56:24 -0400

    sign two

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,20 +1,25 @@
 #include "main.h"
 /**
  * print_sign - Prints the sign of a number
- * @n: the number to be checked
- * Return: 1 for possitive num, -1 for negative num or zero for anything else
+ * @n: the int to check
+ * Return: 1  and prints 0 if n is zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-	_putchar(43);
+	_putchar('+');
 	return (1);
 	}
-	if (n < 0)
-	_putchar(45);
-	return (-1);
-
-	_putchar(48);
-	return (0);
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	} else if (n < 0)
+	{
+		_putchar('-');
+	}
+	return (1);
 }
