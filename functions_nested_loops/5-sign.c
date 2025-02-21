d67e9f08944261359e2a9287ddf386672f142afa
Paola-cmyk <paonicolegonzalez@gmail.com>
2025-02-21 11:45:28 -0400

    sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_sign - Prints the sign of a number
+ * @n: the number to be checked
+ * Return: 1 for possitive num, -1 for negative num or zero for anything else
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar(43);
+	return (1);
+	}
+	if (n < 0)
+	_putchar(45);
+	return (-1);
+
+	_putchar(48);
+	return (0);
+}
