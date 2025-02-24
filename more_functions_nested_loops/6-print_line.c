d489c179ed3643865ba44ccf84c364ec04187f34
Paola-cmyk <paonicolegonzalez@gmail.com>
2025-02-24 14:12:59 -0400

    shortest

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,19 +1,23 @@
 #include "main.h"
+
 /**
- * print_line - draw a straight line in a terminal
- * @n: input character
- *
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character _ should be printed
  */
 void print_line(int n)
 {
-	int i;
-
-	for (i = 0; i < n; i++)
+	if (n <= 0)
 	{
-		if (n > 0)
+		_putchar('\n');
+	} else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
 		{
-			_putchar(' ');
+			_putchar('_');
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
+
 }
