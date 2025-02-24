bbb5f085c079214533726a95777ac833fc936d93
Paola-cmyk <paonicolegonzalez@gmail.com>
2025-02-24 11:18:54 -0400

    mully

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - Multiply two integers
+ * @a: first interger
+ * @b: second interger
+ *Return: Multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
