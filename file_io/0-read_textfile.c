6e4d72198f3f4d8a963af09215fbcb824cde1db2
Paola-cmyk <paonicolegonzalez@gmail.com>
2025-04-06 21:04:34 -0400

    updated zero

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * read_textfile - Reads a text file and prints it to POSIX stdout
  * @filename: A pointer to the name of the file.
  * @letters: The number of letters the
- *  function to read and print.
+ *  function to read and print
+ *  .
  * Return: If the function fails or filename is NULL - 0.
- *  O/w - the actual number of bytes the function can read and print.
+ *  O/w - the actual number of bytes the function can read and print
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
